b8c96a8fce4b3d5e95eae826223bb1dbefd0824d
Sharan Narang <sharannarang@Sharan-Narangs-MacBook-Pro.local>
2014-12-07 20:50:49 -0800

    Compute accuracy for different number of predictions

--- en_US/accuracy/predict_accuracy.R
+++ en_US/accuracy/predict_accuracy.R
@@ -2,8 +2,8 @@ predict_accuracy <- function(input_text, lines, grams=5, preds=5) {
     text <- input_text[1:lines]
     text_grams <- lapply(text, FUN = get_last_n_words, n=grams)
     predictions <- sapply(text_grams, simplify = "array", USE.NAMES=FALSE, FUN=get_prediction, num_preds=preds)
-    sum(predictions)/lines
-    
+    pred_acc<- apply(predictions, 1, sum)/lines   
+    pred_acc
 }
 
 get_last_n_words <- function(text, n=5) {
@@ -22,9 +22,11 @@ get_prediction <- function(clean_words, num_preds) {
     print(paste("Getting Prediction for: "), quote=F)
     print(paste(clean_words[-len]))
     pred <- predict_next_word(clean_words[1:(len-1)], 0, num_preds)
-    res <- clean_words[len] %in% pred
     print("Predictions:", quote=F)
     print(paste(pred))
     print(paste("Actual Word: ", clean_words[len]))
+    res <- c(clean_words[len] %in% head(pred,1), clean_words[len] %in% head(pred,2),
+             clean_words[len] %in% head(pred,3), clean_words[len] %in% head(pred,4),
+             clean_words[len] %in% head(pred,5))
     res
 }
